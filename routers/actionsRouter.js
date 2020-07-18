5556f90b985bbc7f807b78a5650f807bfb3ca98e
aland1013 <adam.land@gmail.com>
2020-07-17 22:39:45 -0400

    finished the POST /api/actions endpoint

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Actions = require('../data/helpers/actionModel');
+const Projects = require('../data/helpers/projectModel');
 
 const router = express.Router();
 
@@ -33,6 +34,45 @@ router.get('/:id', (req, res) => {
         message: 'Error retrieving the action'
       });
     });
+});
+
+/* ----- POST /api/actions ----- */
+router.post('/', (req, res) => {
+  const newAction = req.body;
+
+  if (!newAction.project_id) {
+    res.status(404).json({
+      message: 'Please include a project_id'
+    });
+  } else if (!newAction.description || !newAction.notes) {
+    res.status(404).json({
+      message: 'Please include a description and notes'
+    });
+  } else {
+    Projects.get(newAction.project_id)
+      .then((project) => {
+        if (project) {
+          Actions.insert(newAction)
+            .then((action) => {
+              res.status(201).json(action);
+            })
+            .catch((err) => {
+              res.status(500).json({
+                message: 'Error adding the action'
+              });
+            });
+        } else {
+          res.status(404).json({
+            message: 'Project not found'
+          });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: 'Error retrieving the project'
+        });
+      });
+  }
 });
 
 module.exports = router;
