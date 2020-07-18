b3887cbe5e91a5e8f09bd5c0d9a093265fa452e7
aland1013 <adam.land@gmail.com>
2020-07-17 22:58:27 -0400

    mvp complete

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -96,4 +96,25 @@ router.delete('/:id', (req, res) => {
     });
 });
 
+/* ----- UPDATE /api/actions/:id ----- */
+router.put('/:id', (req, res) => {
+  const changes = req.body;
+
+  Actions.update(req.params.id, changes)
+    .then((action) => {
+      if (action) {
+        res.status(200).json(action);
+      } else {
+        res.status(404).json({
+          message: 'The action could not be found'
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: 'Error updating the action'
+      });
+    });
+});
+
 module.exports = router;
