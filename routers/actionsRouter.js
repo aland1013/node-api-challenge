65904173817dc00a9617569d91de8cbd90186229
aland1013 <adam.land@gmail.com>
2020-07-17 22:45:44 -0400

    finished DELETE /api/actions/:id endpoint

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -75,4 +75,25 @@ router.post('/', (req, res) => {
   }
 });
 
+/* ----- DELETE /api/actions/:id ----- */
+router.delete('/:id', (req, res) => {
+  Actions.remove(req.params.id)
+    .then((count) => {
+      if (count > 0) {
+        res.status(200).json({
+          message: 'The action has been removed'
+        });
+      } else {
+        res.status(404).json({
+          message: 'The action could not be found'
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: 'Error removing the action'
+      });
+    });
+});
+
 module.exports = router;
