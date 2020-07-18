9f5fe8ca0b9c0bea4b1394be450c4810032e6897
aland1013 <adam.land@gmail.com>
2020-07-17 22:12:55 -0400

    finished GET /api/actions/:id endpoint

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -16,4 +16,23 @@ router.get('/', (req, res) => {
     });
 });
 
+/* ----- GET /api/actions/:id ----- */
+router.get('/:id', (req, res) => {
+  Actions.get(req.params.id)
+    .then((action) => {
+      if (action) {
+        res.status(200).json(action);
+      } else {
+        res.status(404).json({
+          message: 'Action not found'
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: 'Error retrieving the action'
+      });
+    });
+});
+
 module.exports = router;
